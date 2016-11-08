e4ec768972e9cce6b0e38bfe8c6a0a4dcec6a609
Jack Palmer <jack.palmer@bbc.co.uk>
2016-11-08 17:56:18 +0000

    Updated note container tests

--- test/containers/note.spec.js
+++ test/containers/note.spec.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import React from 'react';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
+import marked from 'marked';
 import NotePage from '../../app/containers/note';
 import configureStore from '../../app/store/configureStore';
 
@@ -15,8 +16,10 @@ function setup(initialState) {
   );
   return {
     app,
-    edit: app.find('.notes__edit'),
-    view: app.find('.notes__view')
+    edit: app.find('.note__edit'),
+    view: app.find('.note__view'),
+    editViewButton: app.find('.note__header__btn'),
+    title: app.find('.note__header__title')
   };
 }
 
@@ -27,19 +30,71 @@ describe('Note containers', () => {
     expect(app.containsMatchingElement(<NotePage />)).to.equal(true);
   });
 
-  // it('should display edit panel when view in store is set to "edit"', () => {
-  //   const { edit, view } = setup({
-  //     view: 'edit'
-  //   });
-  //   expect(edit.hasClass('hide')).to.be.false;
-  //   expect(view.hasClass('hide')).to.be.true;
-  // });
-  //
-  // it('should display view panel when view in store is set to "view"', () => {
-  //   const { edit, view } = setup({
-  //     view: 'view'
-  //   });
-  //   expect(edit.hasClass('hide')).to.be.true;
-  //   expect(view.hasClass('hide')).to.be.false;
-  // });
+  it('should display edit panel when view in store is set to "edit"', () => {
+    const { edit, view } = setup({
+      note: {
+        title: 'test title',
+        view: 'edit',
+        html: '',
+        raw: ''
+      }
+    });
+    expect(edit.hasClass('hide')).to.be.false;
+    expect(view.hasClass('hide')).to.be.true;
+  });
+
+  it('should display view panel when view in store is set to "view"', () => {
+    const { edit, view } = setup({
+      note: {
+        title: 'test title',
+        view: 'view',
+        html: '',
+        raw: ''
+      }
+    });
+    expect(edit.hasClass('hide')).to.be.true;
+    expect(view.hasClass('hide')).to.be.false;
+  });
+
+  it('should display correct html in panel when switching from \'edit\' view', () => {
+    const { edit, editViewButton, view } = setup({
+      note: {
+        title: 'test title',
+        view: 'edit',
+        html: '',
+        raw: ''
+      }
+    });
+
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: true,
+      smartLists: true,
+      smartypants: false
+    });
+
+    const raw = '**test**';
+    const html = marked(raw);
+    edit.node.value = raw;
+    editViewButton.simulate('click');
+
+    expect(view.html()).to.contain(html);
+  });
+
+  it('should display correct note title from store', () => {
+    const { title } = setup({
+      note: {
+        title: 'test title',
+        view: 'edit',
+        html: '',
+        raw: ''
+      }
+    });
+
+    expect(title.node.value).to.equal('test title');
+  });
 });
